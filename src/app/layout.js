791a9458c5ed384e8dd06260210d450077029b95
Md. Muttalib <mdabdulmottalib2222@gmail.com>
2026-01-04 14:34:42 +0600

    added google font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,28 @@
 import Navbar from "@/components/navbar/Navbar";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer/Footer";
 
+const montSerrat = Montserrat({
+  subsets: ["latin"],
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montSerrat.className}>
       <body>
         {/* navbar */}
         <Navbar></Navbar>
 
         {/* main content */}
 
-        <main className=" bg-linear-to-b from-white to-blue-50">{children}</main>
+        <main className=" bg-linear-to-b from-white to-blue-50">
+          {children}
+        </main>
 
         {/* footer */}
 
-      <Footer></Footer>
+        <Footer></Footer>
       </body>
     </html>
   );
